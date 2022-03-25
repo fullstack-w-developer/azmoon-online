b85683262f610285a16fa85f4e2148f92888681e
mahdi_shariflo <mahdilove279@gmail.com>
2022-03-25 14:43:26 +0330

    fixed url

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -10,7 +10,7 @@ import { toast } from "react-toastify";
 toast.configure();
 const App = React.lazy(() => import("./App"));
 
-axios.defaults.baseURL = "https://azmoononlinenodejs.ap-1.evennode.com/api";
+axios.defaults.baseURL = "http://azmoononlinenodejs.eu-4.evennode.com/api";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
 ReactDOM.render(
